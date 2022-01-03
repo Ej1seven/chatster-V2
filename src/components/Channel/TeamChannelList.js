7b28a9a4c032b7f879c7a809a66a697aa812b195
Ej1seven <erik.jamison.hunter@gmail.com>
2022-01-02 19:27:39 -0600

    edits to splash screen

--- src/components/Channel/TeamChannelList.js
+++ src/components/Channel/TeamChannelList.js
@@ -15,8 +15,8 @@ const AddChannel = ({
 
   return (
     <svg
-      width="14"
-      height="14"
+      width="24"
+      height="24"
       viewBox="0 0 14 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
